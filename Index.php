c031b12468af597e1c68191a5bedd1d8a4ccdc7a
MeneerSDJ <semdejong04@gmail.com>
2023-03-15 13:22:36 +0100

    Implemented php order form in index

--- Index.php
+++ Index.php
@@ -29,8 +29,26 @@
 <div class="submain">
 
 <div class="submain1">
-Main 1
-</div>
+    <form style="max-width: 50rem; margin-left: auto; margin-right: auto;" action="verwerk.php" method="post">
+        <label for="name">Naam:</label>
+        <input type="text" name="name" id="name" />
+        <br />
+        <label for="lastname">Achternaam:</label>
+        <input type="text" name="lastname" id="lastname" />
+        <br />
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" />
+        <br />
+        <label for="product1">Product 1 (Naam & Prijs):</label>
+        <input type="text" name="product1" id="product1" />
+        <input type="number" name="price1" id="price1" />
+        <br />
+        <label for="product2">Product 2 (Naam & Prijs):</label>
+        <input type="text" name="product2" id="product2" />
+        <input type="number" name="price2" id="price2" />
+        <br />
+        <input type="submit" value="Bestel" />
+    </form></div>
 
 <div class="submain2">
 Main 2
