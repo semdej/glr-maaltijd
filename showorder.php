3035a4b010cbe5b9e6e3854ff72cd576992a1573
MeneerSDJ <semdejong04@gmail.com>
2023-03-15 21:00:44 +0100

    Added 3 possible statusses

--- showorder.php
+++ showorder.php
@@ -2,5 +2,11 @@
 
 $orderid = $_POST['ordernummer'];
 
+$iconByStatusList=array("preparing"=>"fa-solid fa-kitchen-set", "delivering"=>"fa-solid fa-person-biking", "delivered"=>"fa-solid fa-pizza-slice");
+$iconByStatus=(object)$iconByStatusList;
+
+$labelByStatusList=array("preparing"=>"Bereiden", "delivering"=>"Onderweg", "delivered"=>"Bezorgd");
+$labelByStatus=(object)$labelByStatusList;
+
 include 'mysql_select.php';
 include 'showorder_view.php';
--- verwerk.php
+++ verwerk.php
@@ -1,38 +1,48 @@
 <?php
 
-    $name = $_POST['name'];
-    $lastname = $_POST['lastname'];
-    $email = $_POST['email'];
-    $product1 = $_POST['pizzas'];
-    $product2 = $_POST['drinks'];
 
-    if ($product1 === 'margarita') {
-        $price1 = '7.80';
-    } elseif ($product1 === 'marinara') {
-        $price1 = '8';
-    } elseif ($product1 === 'capricciosa') {
-        $price1 = '8.30';
-    } elseif ($product1 === 'quattroformaggi') {
-        $price1 = '8';
-    }
+     $name = $_POST['name'];
+     $lastname = $_POST['lastname'];
+     $email = $_POST['email'];
+     $product1 = $_POST['pizzas'];
+     $product2 = $_POST['drinks'];
 
-    if ($product2 === 'cocacola') {
-        $price2 = '2.30';
-    } elseif ($product2 === 'cocacolazero') {
-        $price2 = '2.25';
-    } elseif ($product2 === 'sprite') {
-        $price2 = '2.15';
-    } elseif ($product2 === 'redbull') {
-        $price2 = '3';
-    }
+     if ($product1 === 'margarita') {
+         $price1 = '7.80';
+     } elseif ($product1 === 'marinara') {
+         $price1 = '8';
+     } elseif ($product1 === 'capricciosa') {
+         $price1 = '8.30';
+     } elseif ($product1 === 'quattroformaggi') {
+         $price1 = '8';
+     }
+
+     if ($product2 === 'cocacola') {
+         $price2 = '2.30';
+     } elseif ($product2 === 'cocacolazero') {
+         $price2 = '2.25';
+     } elseif ($product2 === 'sprite') {
+         $price2 = '2.15';
+     } elseif ($product2 === 'redbull') {
+         $price2 = '3';
+     }
+
+     $totaal = $price1 + $price2;
+
+    $iconByStatusList=array("preparing"=>"fa-solid fa-kitchen-set", "delivering"=>"fa-solid fa-person-biking", "delivered"=>"fa-solid fa-pizza-slice");
+    $iconByStatus=(object)$iconByStatusList;
+
+    $labelByStatusList=array("preparing"=>"Bereiden", "delivering"=>"Onderweg", "delivered"=>"Bezorgd");
+    $labelByStatus=(object)$labelByStatusList;
 
 
+     function generateRandomString($length = 10)
+     {
+         return substr(str_shuffle(str_repeat($x = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length / strlen($x)))), 1, $length);
+     }
 
-function generateRandomString($length = 10) {
-        return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
-    }
+     $orderid = generateRandomString();
 
-    $orderid = generateRandomString();
 
     include 'mysql_insert.php';
     include 'mysql_select.php';
