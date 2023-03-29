4e5e368fda6308e03807ade124898b238a941c99
annnnnev <89993@glr.nl>
2023-03-29 13:54:19 +0200

    1

--- bestellen.php
+++ bestellen.php
@@ -11,7 +11,7 @@ $_SESSION['adress'] = $_POST['adress'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="src/css/index.css">
-    <link rel="shortcut icon" href="logo(same folder as html).png" type="image/x-icon">
+    <link rel="shortcut icon" href="src/logo(same folder as html).png" type="image/x-icon">
     <title>Home</title>
 </head>
 <body>
