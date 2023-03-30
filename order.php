42c82a67e0a725037f373ac8cbe0d2089f6d9b36
MeneerSDJ <semdejong04@gmail.com>
2023-03-30 14:27:31 +0200

    Cleaned up files

--- order.php
+++ order.php
@@ -1,15 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/css/order.css">
+    <link rel="shortcut icon" href="src/pic/logo.png" type="image/x-icon">
     <title>Bestelling Bekijken</title>
 </head>
 <body>
-<form style="max-width: 50rem; margin-left: auto; margin-right: auto;" action="showorder.php" method="post">
-    <label for="ordernummer">Ordernummer:</label>
-    <input type="text" name="ordernummer" id="ordernummer" required />
-    <br />
-    <input type="submit" value="Bekijken" />
-</form>
+<div class="container">
+
+
+    <div class="header">
+        <a href="index.php"><div class="header1"><img src="src/pic/logo.png" alt="logo" class="logo"></div></a>
+        <div class="header2">
+            <a id="knop" href="order.php">Order</a>
+            <a id="knop" href="menu.php">Menu</a>
+            <a id="knop1" href="bestellen.php">Bestellen</a>
+        </div>
+    </div>
+
+    <div class="main">
+        <div class="logofront"><a></a></div>
+        <div class="maint">Order</div>
+    </div>
+
+
+    <div class="submain">
+        <form style="max-width: 50rem; margin-left: auto; margin-right: auto;" action="showorder.php" method="post">
+            <label for="ordernummer">Ordernummer:</label>
+            <input type="text" name="ordernummer" id="ordernummer" required />
+            <br />
+            <input type="submit" value="Bekijken" />
+        </form>
+        </div>
+    </div>
+
+
+    <div class="footer">
+        <a>Copyright © 2023 - SignorePizza®. All rights reserved.</a>
+    </div>
+
 </body>
 </html>
--- verwerk.php
+++ verwerk.php
@@ -1,49 +1,49 @@
 <?php
-
     session_start();
 
-    $adress = $_SESSION['adress'];
-    $name = $_POST['name'];
-    $lastname = $_POST['lastname'];
-    $email = $_POST['email'];
-    $product1 = $_POST['pizzas'];
-    $product2 = $_POST['drinks'];
+     $adress = $_SESSION['adress'];
+     $name = $_POST['name'];
+     $lastname = $_POST['lastname'];
+     $email = $_POST['email'];
+     $product1 = $_POST['pizzas'];
+     $product2 = $_POST['drinks'];
 
-    if ($product1 === 'margarita') {
-        $price1 = '7.80';
-    } elseif ($product1 === 'marinara') {
-        $price1 = '8';
-    } elseif ($product1 === 'capricciosa') {
-        $price1 = '8.30';
-    } elseif ($product1 === 'quattroformaggi') {
-        $price1 = '8';
-    }
+     if ($product1 === 'margarita') {
+         $price1 = '7.80';
+     } elseif ($product1 === 'marinara') {
+         $price1 = '8';
+     } elseif ($product1 === 'capricciosa') {
+         $price1 = '8.30';
+     } elseif ($product1 === 'quattroformaggi') {
+         $price1 = '8';
+     }
 
-    if ($product2 === 'cocacola') {
-        $price2 = '2.30';
-    } elseif ($product2 === 'cocacolazero') {
-        $price2 = '2.25';
-    } elseif ($product2 === 'sprite') {
-        $price2 = '2.15';
-    } elseif ($product2 === 'redbull') {
-        $price2 = '3';
-    }
+     if ($product2 === 'cocacola') {
+         $price2 = '2.30';
+     } elseif ($product2 === 'cocacolazero') {
+         $price2 = '2.25';
+     } elseif ($product2 === 'sprite') {
+         $price2 = '2.15';
+     } elseif ($product2 === 'redbull') {
+         $price2 = '3';
+     }
 
-    $totaal = $price1 + $price2;
+     $totaal = $price1 + $price2;
 
-    $imgByStatusList = array("preparing" => "src/pic/food.gif", "delivering" => "src/pic/bike.gif", "delivered" => "src/pic/star.gif");
-    $imgByStatus = (object)$imgByStatusList;
+    $imgByStatusList=array("preparing"=>"src/pic/food.gif", "delivering"=>"src/pic/bike.gif", "delivered"=>"src/pic/star.gif");
+    $imgByStatus=(object)$imgByStatusList;
 
-    $labelByStatusList = array("preparing" => "Uw bestelling wordt op dit moment bereid", "delivering" => "Uw bestelling is onderweg", "delivered" => "Uw bestelling is bezorgd, eet smakelijk!");
-    $labelByStatus = (object)$labelByStatusList;
+    $labelByStatusList=array("preparing"=>"Uw bestelling wordt op dit moment bereid", "delivering"=>"Uw bestelling is onderweg", "delivered"=>"Uw bestelling is bezorgd, eet smakelijk!");
+    $labelByStatus=(object)$labelByStatusList;
 
 
-    function generateRandomString($length = 10)
-    {
-        return substr(str_shuffle(str_repeat($x = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length / strlen($x)))), 1, $length);
-    }
+     function generateRandomString($length = 10)
+     {
+         return substr(str_shuffle(str_repeat($x = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length / strlen($x)))), 1, $length);
+     }
 
-    $orderid = generateRandomString();
+     $orderid = generateRandomString();
+
 
     include 'mysql_insert.php';
     include 'mysql_select.php';
