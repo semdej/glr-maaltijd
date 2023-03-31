465d711d771a2ff8ab56b57ea97a84ee2b814123
annnnnev <89993@glr.nl>
2023-04-01 00:03:46 +0200

    homepage done

--- Index.php
+++ Index.php
@@ -42,7 +42,49 @@
 </div>
 
 <div class="submain2">
-Main 2
+    <h1>Bezorgen</h1>
+    <p>Geen zin om de deur uit te gaan? <br>
+        Bestel snel en eenvoudig op onze eigen website
+        en wij zorgen dat het eten wordt bezorgd op het
+        door jou gekozen tijdstip. Wil je direct kunnen genieten
+        van je maaltijd? Betaal direct online snel en veilig met iDEAL,
+        zo hoef je aan de deur<br>niets meer af te rekenen.</p>
+    <div>
+        <h1>Bezorgtijden</h1>
+        <table>
+            <tr>
+                <td>maandag:</td>
+                <td>15:45-00:40</td>
+            </tr>
+            <tr>
+                <td>dinsdag:</td>
+                <td>15:45-00:40</td>
+            </tr>
+            <tr>
+                <td>woensdag:</td>
+                <td>15:45-00:40</td>
+            </tr>
+            <tr>
+                <td>donderdag:</td>
+                <td>15:45-00:40</td>
+            </tr>
+            <tr>
+                <td>vrijdag:</td>
+                <td>15:45-01:40</td>
+            </tr>
+            <tr>
+                <td>zaterdag:</td>
+                <td>15:45-01:40</td>
+            </tr>
+            <tr>
+                <td>zondag:</td>
+                <td>15:45-00:40</td>
+            </tr>
+        </table>
+    </div>
+    <br><br>
+    <h1 class="locatie">Locatie</h1>
+    <p>Rietdekkerweg 131<br>3068 GW Rotterdam<br>010 220 0114</p>
 </div>
 
 </div>
